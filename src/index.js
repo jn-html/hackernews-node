c52b60f43549bc46ff1a117203ca7364ddd7887a
jn-html <marmainjean@gmail.com>
2020-01-27 13:46:23 +0100

    Add comments for explanations in src/index.js file

--- src/index.js
+++ src/index.js
@@ -27,3 +27,39 @@ const server = new GraphQLServer ({
     resolvers,
 })
 server.start(() => console.log(`Server is running on localhost:4000`))
+
+// typeDefs: Ce sont les définitions de type de votre schéma d'application.
+
+// resolvers: Il s'agit d'un objet JavaScript qui reflète les types Query, 
+// Mutationet Subscriptionet leurs champs à partir de votre schéma d'application. 
+// Chaque champ du schéma d'application est représenté par une fonction portant le même nom dans cet objet.
+
+//context: Il s'agit d'un objet qui est transmis à travers la chaîne de résolveurs et chaque résolveur peut lire ou écrire dans.
+
+// # Query for all users
+// query {
+//   users {
+//     id --- ici on interroge id & name, possible d'en omettre 1 des 2
+//     name
+//   }
+// }
+
+// # Query a single user by their id
+// query {
+//   user(id: "user-1") {
+//     id
+//     name
+//   }
+// }
+
+// # Create a new user
+// mutation {
+//   createUser(name: "Bob") {
+//     id
+//     name
+//   }
+// }
+
+// Lorsque vous fournissez ces informations, 
+// l'instance Prisma aura un accès complet à votre service de base de données 
+// et peut être utilisée pour résoudre la demande entrante ultérieurement.
